01808d9f39c1e8bf38284c895106fe8d2ad312a9
Manish Munikar <munikarmanish@gmail.com>
2017-06-14 19:35:33 +0545

    added other properties in Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,51 @@
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Task {
 
+    private int mId;
     private String mDescription;
+    private boolean mCompleted;
+    private LocalDateTime mCreatedAt;
+
+    private static List<Task> sInstances = new ArrayList<Task>();
 
     // constructor
     public Task(String description) {
         mDescription = description;
+        mCompleted = false;
+        mCreatedAt = LocalDateTime.now();
+
+        sInstances.add(this);
+        mId = sInstances.size();
+    }
+
+    public int getId() {
+        return mId;
     }
 
     public String getDescription() {
         return mDescription;
+    }
+
+    public boolean isCompleted() {
+        return mCompleted;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return mCreatedAt;
+    }
+
+    public static List<Task> all() {
+        return sInstances;
+    }
+
+    public static void clear() {
+        sInstances.clear();
+    }
+
+    public static Task find(int id) {
+        return sInstances.get(id-1);
     }
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 
 public class TaskTest {
@@ -14,5 +15,50 @@ public class TaskTest {
     public void Task_instantiatesWithDescription() {
         Task t = new Task("Mow the lawn");
         assertEquals("Mow the lawn", t.getDescription());
+    }
+
+    @Test
+    public void Task_is_incomplete_when_created() {
+        Task t = new Task("Mow the lawn");
+        assertEquals(false, t.isCompleted());
+    }
+
+    @Test
+    public void Task_is_created_with_current_timestamp() {
+        Task t = new Task("Mow the lawn");
+        assertEquals(LocalDateTime.now().getDayOfWeek(), t.getCreatedAt().getDayOfWeek());
+    }
+
+    @Test
+    public void all_returns_all_instances_of_Task() {
+        Task t1 = new Task("Mow the lawn");
+        Task t2 = new Task("Buy groceries");
+        assertEquals(true, Task.all().contains(t1));
+        assertEquals(true, Task.all().contains(t2));
+    }
+
+    @Test
+    public void clear_all_tasks() {
+        Task t1 = new Task("Mow the lawn");
+        Task t2 = new Task("Buy groceries");
+        Task.clear();
+        assertEquals(0, Task.all().size());
+    }
+
+    @Test
+    public void tasks_are_created_with_IDs() {
+        Task.clear();
+        Task t1 = new Task("Mow the lawn");
+        assertEquals(1, t1.getId());
+        Task t2 = new Task("Buy groceries");
+        assertEquals(2, t2.getId());
+    }
+
+    @Test
+    public void find_Task_with_ID() {
+        Task t1 = new Task("Mow the lawn");
+        Task t2 = new Task("Buy groceries");
+        assertEquals(Task.find(t2.getId()), t2);
+        assertEquals(Task.find(t1.getId()), t1);
     }
 }
